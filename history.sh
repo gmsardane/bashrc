73703128a3fa2c4bcf0c1cb2fe0094977ac46864
Paul Mooring <paul@opscode.com>
2012-12-01 12:05:36 -0700

    Added history.sh to sync history across multiple terminals

---
+++ history.sh
@@ -0,0 +1,17 @@
+export HISTSIZE=9000
+export HISTFILESIZE=$HISTSIZE
+export HISTCONTROL=ignorespace:ignoredups
+
+history() {
+  _bash_history_sync
+  builtin history "$@"
+}
+
+_bash_history_sync() {
+  builtin history -a          # Write to history file
+  HISTFILESIZE=$HISTFILESIZE  # Truncate history file
+  builtin history -c          # Clear session history
+  builtin history -r          # Reload session history
+}
+
+PROMPT_COMMAND="_bash_history_sync ; $PROMPT_COMMAND"
