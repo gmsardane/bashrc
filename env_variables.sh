4eb583e13150ca843e86a8b8d9dc15999a00f6bb
Paul Mooring <paul@opscode.com>
2012-12-13 13:35:26 -0700

    Added /usr/local/bin to front of PATH

--- env_variables.sh
+++ env_variables.sh
@@ -1,9 +1,8 @@
 #### Environmental variables ####
 
-export PATH="${PATH}:/usr/local/sbin"
+export PATH="/usr/local/bin:${PATH}"
 export EDITOR="subl --wait"
 export CLICOLOR=1
 export JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Home/"
 
 export ARCHFLAGS="-arch x86_64"
-export CC=/usr/local/bin/gcc-4.7
