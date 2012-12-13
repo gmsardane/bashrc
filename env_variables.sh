018535d80817083c2409791f5c78abad8e19d655
Paul Mooring <paul@opscode.com>
2012-12-12 20:53:24 -0700

    Added CC stuff to env_variables.sh

--- env_variables.sh
+++ env_variables.sh
@@ -2,4 +2,8 @@
 
 export PATH="${PATH}:/usr/local/sbin"
 export EDITOR="subl --wait"
+export CLICOLOR=1
 export JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Home/"
+
+export ARCHFLAGS="-arch x86_64"
+export CC=/usr/local/bin/gcc-4.7
